f31e7b699c63737033dc95443e7c39eecb2ec45d
Antoine POPINEAU <antoine.popineau@appscho.com>
2017-08-07 17:01:28 +0200

    Added a few tests.

---
+++ util_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringArrayContains(t *testing.T) {
+	arr := []string{"lorem", "ipsum", "dolor", "sit", "amet"}
+
+	assert.True(t, StringArrayContains(arr, "ipsum"), "array should contain 'ipsum'")
+	assert.False(t, StringArrayContains(arr, "engage"), "array should not contain 'engage'")
+}
+
+func TestRemoveFromSlice(t *testing.T) {
+	arr := []string{"lorem", "ipsum", "dolor", "sit", "amet"}
+	newArr := RemoveFromSlice(arr, "ipsum")
+
+	assert.Equal(t, 4, len(newArr), "array should contain one less element")
+	assert.False(t, StringArrayContains(newArr, "ipsum"))
+}
