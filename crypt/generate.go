5167005f42b4ff5f90ae38010cec26e317ecab6f
Antoine POPINEAU <antoine.popineau@appscho.com>
2017-08-14 18:42:49 +0200

    Adding ASCII symbols to password generation.

--- crypt/generate.go
+++ crypt/generate.go
@@ -6,13 +6,20 @@ import (
 )
 
 var basicChars = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+var symbolChars = []rune(`!"#$%&'()*+,-./:;<=>?@[\]$^_{|}~`)
 
 func GeneratePassword(length int) string {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	password := make([]rune, length)
 	for i := range password {
-		password[i] = basicChars[rand.Intn(len(basicChars))]
+		var char rune
+		if rand.Intn(4) < 1 {
+			char = symbolChars[rand.Intn(len(symbolChars))]
+		} else {
+			char = basicChars[rand.Intn(len(basicChars))]
+		}
+		password[i] = char
 	}
 
 	return string(password)
